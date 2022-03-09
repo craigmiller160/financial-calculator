8687863d43a00d53e2703f66acc15e5e7098b51e
Craig Miller <craigmiller160@gmail.com>
2022-03-09 11:26:36 -0500

    Logging final result of execution

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { getData } from './data';
+import { pipe } from 'fp-ts/function';
+import * as IOEither from 'fp-ts/IOEither';
+import { logger } from './logger';
+import * as IO from 'fp-ts/IO';
 
-getData()();
+pipe(
+	getData(),
+	IOEither.fold(
+		(ex) => logger.errorWithStack('Error calculating 401k', ex),
+		(output) =>
+			pipe(
+				logger.info('Successfully calculated 401k'),
+				IO.chain(() => logger.info(output.toString()))
+			)
+	)
+)();
