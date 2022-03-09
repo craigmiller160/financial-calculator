22f0827c3f1d6982cbafbc5cde8fdc44cd887c1a
Craig Miller <craigmiller160@gmail.com>
2022-03-09 14:24:14 -0500

    Put in safety switch to prevent future income glitch from breaking everything

--- src/calculations/calculateFuture401k.ts
+++ src/calculations/calculateFuture401k.ts
@@ -1,5 +1,8 @@
 import * as Loop from '../function/Loop';
 import Decimal from 'decimal.js';
+import { PredicateT } from '@craigmiller160/ts-functions/types';
+import * as Pred from 'fp-ts/Predicate';
+import { pipe } from 'fp-ts/function';
 
 interface Context {
 	readonly remainingAmount401k: Decimal;
@@ -10,6 +13,11 @@ interface Context {
 
 // 0.1%
 const INTERVAL = new Decimal(0.001);
+
+const noAmount401k: PredicateT<Context> = (ctx) =>
+	ctx.amount401k.eq(new Decimal(0));
+const tooHighAmount401k: PredicateT<Context> = (ctx) =>
+	ctx.amount401k.comparedTo(ctx.remainingAmount401k) > 0;
 
 export const calculateFuture401k = (
 	remainingAmount401k: Decimal,
@@ -22,7 +30,7 @@ export const calculateFuture401k = (
 		amount401k: new Decimal(0)
 	};
 	const resultContext = Loop.runUntil<Context>(
-		(ctx) => ctx.amount401k.comparedTo(ctx.remainingAmount401k) > 0
+		pipe(noAmount401k, Pred.or(tooHighAmount401k))
 	)((ctx) => {
 		const newRate = ctx.rate401k.plus(INTERVAL);
 		const newAmount = ctx.totalFutureIncome.times(newRate);
