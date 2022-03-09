b1bd70e3239f7a390fded8fc12a6b9c52c049a92
Craig Miller <craigmiller160@gmail.com>
2022-03-09 13:44:32 -0500

    Stubbed out 401k calculations

---
+++ src/calculations/calculateFuture401k.ts
@@ -0,0 +1,4 @@
+
+export const calculateFuture401k = (contributionLimit401k: number, totalFutureIncome: number): [rate: number, amount: number] => {
+    throw new Error();
+};
--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -1,9 +1,14 @@
 import { Data } from '../data/decoders';
 import { calculatePastData } from './calculatePastData';
 import { calculateFutureData } from './calculateFutureData';
+import { calculateFuture401k } from './calculateFuture401k';
 
 export const runCalculations = (data: Data): string => {
 	const pastData = calculatePastData(data);
 	const futureData = calculateFutureData(data);
-	return `${pastData} ${futureData}`;
+	const [futureRate401k, futureAmount401k] = calculateFuture401k(
+		data.legalData.contributionLimit401k,
+		futureData.totalIncome
+	);
+	return `${pastData} ${futureData} ${futureRate401k} ${futureAmount401k}`;
 };
