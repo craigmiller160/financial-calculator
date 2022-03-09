458d9c036b2c722d4954780d6c067f0d821fc6db
Craig Miller <craigmiller160@gmail.com>
2022-03-09 13:06:17 -0500

    Added eslint-disable

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -2,6 +2,7 @@ import { PersonalData } from '../data/decoders';
 import { calculatePastData } from './calculatePastData';
 import { calculateFutureData } from './calculateFutureData';
 
+/* eslint-disable  */
 export const runCalculations = (data: PersonalData): string => {
 	const pastData = calculatePastData(data);
 	const futureData = calculateFutureData(data);
