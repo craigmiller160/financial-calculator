9fd010832615411d4a18ce37ea3a1cd3e97d32a4
Craig Miller <craigmiller160@gmail.com>
2022-03-09 13:58:26 -0500

    Stubbed out test

---
+++ test/calculations/calculateFuture401k.test.ts
@@ -0,0 +1,7 @@
+import { calculateFuture401k } from '../../src/calculations/calculateFuture401k';
+
+describe('calculateFuture401k', () => {
+	it('accurately does calculation', () => {
+		throw new Error();
+	});
+});
