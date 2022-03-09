850fd67e7b7ba338726d92fd0d4de9d1ce2592bc
Craig Miller <craigmiller160@gmail.com>
2022-03-09 15:58:40 -0500

    Stubbed out new tests

---
+++ test/calculations/calculateFutureData.test.ts
@@ -0,0 +1,7 @@
+export {};
+
+describe('calculateFutureData', () => {
+	it('performs the calculations', () => {
+		throw new Error();
+	});
+});
---
+++ test/calculations/calculatePastData.test.ts
@@ -0,0 +1,7 @@
+export {};
+
+describe('calculatePastData', () => {
+	it('performs the calculations', () => {
+		throw new Error();
+	});
+});
