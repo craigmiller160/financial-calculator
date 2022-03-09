8e1fb6b35e5c8acfe63273ece615f960e48aa142
Craig Miller <craigmiller160@gmail.com>
2022-03-09 16:08:40 -0500

    Implemented test

--- test/calculations/calculatePastData.test.ts
+++ test/calculations/calculatePastData.test.ts
@@ -1,7 +1,23 @@
-export {};
+import { getTestData } from '../testutils/TestData';
+import { pipe } from 'fp-ts/function';
+import * as IOEither from 'fp-ts/IOEither';
+import { calculatePastData } from '../../src/calculations/calculatePastData';
+import '@relmify/jest-fp-ts';
 
 describe('calculatePastData', () => {
 	it('performs the calculations', () => {
-		throw new Error();
+		const result = pipe(
+			getTestData(),
+			IOEither.map(calculatePastData),
+			IOEither.map((decimalResult) => ({
+				total401kContribution:
+					decimalResult.total401kContribution.toFixed(2),
+				totalTaxableIncome: decimalResult.totalTaxableIncome.toFixed(2)
+			}))
+		)();
+		expect(result).toEqualRight({
+			total401kContribution: '3950.61',
+			totalTaxableIncome: '26597.86'
+		});
 	});
 });
