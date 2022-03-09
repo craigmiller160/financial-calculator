9b65a12652b7b029517ed3d537a183e8c0e6251e
Craig Miller <craigmiller160@gmail.com>
2022-03-09 13:42:27 -0500

    Eliminated temp eslint-disable

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -2,9 +2,8 @@ import { Data } from '../data/decoders';
 import { calculatePastData } from './calculatePastData';
 import { calculateFutureData } from './calculateFutureData';
 
-/* eslint-disable  */
 export const runCalculations = (data: Data): string => {
 	const pastData = calculatePastData(data);
 	const futureData = calculateFutureData(data);
-	return '';
+	return `${pastData} ${futureData}`;
 };
