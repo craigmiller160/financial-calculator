21cd37b949d65afbf6b6fdb0416247ec2493c016
Craig Miller <craigmiller160@gmail.com>
2022-03-10 11:08:03 -0500

    Fixed another test

--- test/calculations/calculateFuture401k.test.ts
+++ test/calculations/calculateFuture401k.test.ts
@@ -6,11 +6,7 @@ const TOTAL_FUTURE_INCOME = new Decimal(10_000);
 
 describe('calculateFuture401k', () => {
 	it('accurately does calculation', () => {
-		const [rate, amount] = calculateFuture401k(
-			REMAINING_AMOUNT,
-			TOTAL_FUTURE_INCOME
-		);
+		const rate = calculateFuture401k(REMAINING_AMOUNT, TOTAL_FUTURE_INCOME);
 		expect(rate).toEqual(new Decimal(0.1));
-		expect(amount).toEqual(new Decimal(1000));
 	});
 });
