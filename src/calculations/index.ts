107e409f42101c7d547cf62e5beba5c850f89309
Craig Miller <craigmiller160@gmail.com>
2022-03-10 14:57:33 -0500

    Cleaned up old code

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -7,48 +7,6 @@ import { addTotalsToData } from './totals/addTotalsToData';
 import { DataWithTotals } from './totals/TotalTypes';
 import { addFuture401k } from './401k/addFuture401k';
 
-// const runPastDataCalculation = (data: Data): IOT<PastData> =>
-// 	pipe(
-// 		logger.debug('Calculating past data'),
-// 		IO.map(() => calculatePastData(data))
-// 	);
-//
-// const runFutureDataCalculation = (data: Data): IOT<FutureData> =>
-// 	pipe(
-// 		logger.debug('Calculating future data'),
-// 		IO.map(() => calculateFutureData(data))
-// 	);
-//
-// const runFuture401kCalculation = (
-// 	data: Data,
-// 	pastData: PastData,
-// 	futureData: FutureData
-// ): IOT<Decimal> =>
-// 	pipe(
-// 		logger.debug('Calculating future 401k contribution'),
-// 		IO.map(() => {
-// 			const remainingAmount401k = new Decimal(
-// 				data.legalData.contributionLimit401k
-// 			).minus(pastData.total401kContribution);
-// 			return calculateFuture401k(
-// 				remainingAmount401k,
-// 				futureData.totalIncome
-// 			);
-// 		})
-// 	);
-//
-// const runCalculationsFor401k = (data: Data): IOT<Calculations401k> =>
-// 	pipe(
-// 		runPastDataCalculation(data),
-// 		IO.bindTo('pastData'),
-// 		IO.bind('futureData', () => runFutureDataCalculation(data)),
-// 		IO.bind('future401kRate', ({ pastData, futureData }) =>
-// 			runFuture401kCalculation(data, pastData, futureData)
-// 		)
-// 	);
-
-// TODO delete a lot of old calculations above
-
 const runCalculationsForTotals = (data: Data): IOT<DataWithTotals> =>
 	pipe(
 		logger.debug('Calculating total values for data'),
