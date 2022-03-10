16179639dd3e16cd1df471deeedd4a3afcd22a2e
Craig Miller <craigmiller160@gmail.com>
2022-03-10 13:59:03 -0500

    More progress integrating 401k into future pay info

--- src/calculations/401k/addFuture401k.ts
+++ src/calculations/401k/addFuture401k.ts
@@ -1,6 +1,9 @@
-import { DataWithTotals } from '../totals/TotalTypes';
+import { DataWithTotals, PaycheckWithTotal } from '../totals/TotalTypes';
 import Decimal from 'decimal.js';
 import { PredicateT } from '@craigmiller160/ts-functions/types';
+import { pipe } from 'fp-ts/function';
+import * as Pred from 'fp-ts/Predicate';
+import * as RArray from 'fp-ts/ReadonlyArray';
 
 interface Context {
 	readonly remainingAmount401k: Decimal;
@@ -12,10 +15,61 @@ interface Context {
 // 0.1%
 const INTERVAL = new Decimal(0.001);
 
+// TODO add tests for both of these scenarios
 const noAmount401k: PredicateT<Context> = (ctx) =>
 	ctx.amount401k.eq(new Decimal(0));
 const tooHighAmount401k: PredicateT<Context> = (ctx) =>
 	ctx.amount401k.comparedTo(ctx.remainingAmount401k) > 0;
+const isOver401kLimit: PredicateT<Context> = pipe(
+	noAmount401k,
+	Pred.or(tooHighAmount401k)
+);
+
+const findRate401k = (context: Context): Decimal => {
+	const newRate = context.rate401k.plus(INTERVAL);
+	const newAmount = context.totalFutureIncome.times(newRate);
+	const newContext = {
+		...context,
+		newAmount,
+		newRate
+	};
+	if (isOver401kLimit(newContext)) {
+		return newRate.minus(INTERVAL);
+	}
+	return findRate401k(newContext);
+};
+
+const add401kToPaycheck =
+	(rate: Decimal) =>
+	(paycheck: PaycheckWithTotal): PaycheckWithTotal => {
+		const amount401k = new Decimal(paycheck.grossPay)
+			.times(rate)
+			.toNumber();
+		const taxablePay = new Decimal(paycheck.taxablePay)
+			.minus(new Decimal(amount401k))
+			.toNumber();
+		return {
+			...paycheck,
+			paycheck401k: {
+				rate: rate.toNumber(),
+				amount: amount401k
+			},
+			taxablePay,
+			totalsForAllChecks: {
+				...paycheck.totalsForAllChecks,
+				taxablePay: new Decimal(taxablePay)
+					.times(paycheck.numberOfChecks)
+					.toNumber(),
+				contribution401k: new Decimal(amount401k)
+					.times(paycheck.numberOfChecks)
+					.toNumber()
+			},
+			annualized: {
+				...paycheck.annualized,
+				taxablePay: new Decimal(taxablePay).times(26).toNumber()
+			}
+		};
+	};
 
 export const addFuture401k = (data: DataWithTotals): DataWithTotals => {
 	const remainingAmount401k = new Decimal(
@@ -31,4 +85,9 @@ export const addFuture401k = (data: DataWithTotals): DataWithTotals => {
 		rate401k: new Decimal(0),
 		amount401k: new Decimal(0)
 	};
+	const rate = findRate401k(initContext);
+	const futurePaychecks = pipe(
+		data.personalData.futurePaychecks,
+		RArray.map(add401kToPaycheck(rate))
+	);
 };
