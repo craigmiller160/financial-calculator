a1f44de9ccb38def36fc34e2529772485ec37291
Craig Miller <craigmiller160@gmail.com>
2022-03-10 10:35:03 -0500

    Implemented some tests

--- test/calculations/totals/common.test.ts
+++ test/calculations/totals/common.test.ts
@@ -1,12 +1,24 @@
-export {};
+import { Rate401k } from '../../../src/data/decoders';
+import {
+	getAmount401k,
+	getRate401k
+} from '../../../src/calculations/totals/common';
+
+const valueWith401k: Rate401k = {
+	rate401k: 10
+};
 
 describe('totals common', () => {
 	it('getRate401k', () => {
-		throw new Error();
+		const resultWith401k = getRate401k(valueWith401k);
+		const resultWithout401k = getRate401k({ foo: 'bar' });
+		expect(resultWith401k).toEqual(10);
+		expect(resultWithout401k).toEqual(0);
 	});
 
 	it('getAmount401k', () => {
-		throw new Error();
+		const result = getAmount401k(175.2, 0.15);
+		expect(result).toEqual(26.28);
 	});
 
 	it('getPayrollTaxCosts', () => {
