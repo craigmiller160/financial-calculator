5a201a75cf464b7484049c8d37c37e5ec1540f66
Craig Miller <craigmiller160@gmail.com>
2022-03-10 14:14:50 -0500

    Fixed lint issues

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -1,10 +1,5 @@
 import { Data } from '../data/decoders';
-import { calculatePastData } from './calculatePastData';
-import { calculateFutureData } from './calculateFutureData';
-import { calculateFuture401k } from './calculateFuture401k';
-import Decimal from 'decimal.js';
 import { IOT } from '@craigmiller160/ts-functions/types';
-import { Calculations401k, FutureData, PastData } from './CalculationTypes';
 import { pipe } from 'fp-ts/function';
 import { logger } from '../logger';
 import * as IO from 'fp-ts/IO';
@@ -12,45 +7,45 @@ import { addTotalsToData } from './totals/addTotalsToData';
 import { DataWithTotals } from './totals/TotalTypes';
 import { addFuture401k } from './401k/addFuture401k';
 
-const runPastDataCalculation = (data: Data): IOT<PastData> =>
-	pipe(
-		logger.debug('Calculating past data'),
-		IO.map(() => calculatePastData(data))
-	);
-
-const runFutureDataCalculation = (data: Data): IOT<FutureData> =>
-	pipe(
-		logger.debug('Calculating future data'),
-		IO.map(() => calculateFutureData(data))
-	);
-
-const runFuture401kCalculation = (
-	data: Data,
-	pastData: PastData,
-	futureData: FutureData
-): IOT<Decimal> =>
-	pipe(
-		logger.debug('Calculating future 401k contribution'),
-		IO.map(() => {
-			const remainingAmount401k = new Decimal(
-				data.legalData.contributionLimit401k
-			).minus(pastData.total401kContribution);
-			return calculateFuture401k(
-				remainingAmount401k,
-				futureData.totalIncome
-			);
-		})
-	);
-
-const runCalculationsFor401k = (data: Data): IOT<Calculations401k> =>
-	pipe(
-		runPastDataCalculation(data),
-		IO.bindTo('pastData'),
-		IO.bind('futureData', () => runFutureDataCalculation(data)),
-		IO.bind('future401kRate', ({ pastData, futureData }) =>
-			runFuture401kCalculation(data, pastData, futureData)
-		)
-	);
+// const runPastDataCalculation = (data: Data): IOT<PastData> =>
+// 	pipe(
+// 		logger.debug('Calculating past data'),
+// 		IO.map(() => calculatePastData(data))
+// 	);
+//
+// const runFutureDataCalculation = (data: Data): IOT<FutureData> =>
+// 	pipe(
+// 		logger.debug('Calculating future data'),
+// 		IO.map(() => calculateFutureData(data))
+// 	);
+//
+// const runFuture401kCalculation = (
+// 	data: Data,
+// 	pastData: PastData,
+// 	futureData: FutureData
+// ): IOT<Decimal> =>
+// 	pipe(
+// 		logger.debug('Calculating future 401k contribution'),
+// 		IO.map(() => {
+// 			const remainingAmount401k = new Decimal(
+// 				data.legalData.contributionLimit401k
+// 			).minus(pastData.total401kContribution);
+// 			return calculateFuture401k(
+// 				remainingAmount401k,
+// 				futureData.totalIncome
+// 			);
+// 		})
+// 	);
+//
+// const runCalculationsFor401k = (data: Data): IOT<Calculations401k> =>
+// 	pipe(
+// 		runPastDataCalculation(data),
+// 		IO.bindTo('pastData'),
+// 		IO.bind('futureData', () => runFutureDataCalculation(data)),
+// 		IO.bind('future401kRate', ({ pastData, futureData }) =>
+// 			runFuture401kCalculation(data, pastData, futureData)
+// 		)
+// 	);
 
 // TODO delete a lot of old calculations above
 
@@ -67,8 +62,8 @@ const runCalculationsForFuture401k = (
 	pipe(
 		logger.debug('Calculating future 401k rates and amounts'),
 		IO.map(() => addFuture401k(data)),
-		IO.chainFirst((data) =>
-			logger.debugWithJson('Data with Future 401k', data)
+		IO.chainFirst((dataWithTotals) =>
+			logger.debugWithJson('Data with Future 401k', dataWithTotals)
 		)
 	);
 
