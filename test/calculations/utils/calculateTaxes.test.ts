6a529e68c778a87be3dd9ff29aa512bdf8bb28e8
Craig Miller <craigmiller160@gmail.com>
2022-03-11 13:01:41 -0500

    Implemented another test

--- test/calculations/utils/calculateTaxes.test.ts
+++ test/calculations/utils/calculateTaxes.test.ts
@@ -1,7 +1,17 @@
-export {};
+import { FederalTaxBracket } from '../../../src/data/decoders';
+import { calculateTaxes } from '../../../src/calculations/utils/calculateTaxes';
+import Decimal from 'decimal.js';
+
+const bracket: FederalTaxBracket = {
+	rate: 0.22,
+	minimumIncome: 20_000,
+	maximumIncome: 50_000,
+	baseAmountOwed: 5_000
+};
 
 describe('calculateTaxes', () => {
 	it('does the calculation', () => {
-		throw new Error();
+		const result = calculateTaxes(new Decimal(36_000))(bracket);
+		expect(result).toEqual(new Decimal(8520));
 	});
 });
