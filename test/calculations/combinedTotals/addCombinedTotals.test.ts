19a88839f5e97bc5ac4457218c4d02e94cafd18e
Craig Miller <craigmiller160@gmail.com>
2022-03-11 19:42:39 -0500

    Fixing some tests

--- test/calculations/combinedTotals/addCombinedTotals.test.ts
+++ test/calculations/combinedTotals/addCombinedTotals.test.ts
@@ -10,16 +10,27 @@ describe('addCombinedTotals', () => {
 		const result = addCombinedTotals(inputData);
 		const expectedResult = produce(inputData, (draft) => {
 			draft.totals = {
-				pastGrossPay: 33710.15,
-				pastContribution401k: 7079.1315,
-				pastEstimatedAGI: 23469.342025,
-				pastEstimatedMAGI: 23469.342025,
-				futureGrossPay: 121153.83,
-				futureContribution401k: 13326.9213,
-				futureEstimatedAGI: 97330.98070499998,
-				futureEstimatedMAGI: 97330.98070499998,
-				pastEstimatedTakeHomePay: 250,
-				futureEstimatedTakeHomePay: 100
+				past: {
+					grossPay: 33710.15,
+					contribution401k: 7079.1315,
+					estimatedAGI: 23469.342025,
+					estimatedMAGI: 23469.342025,
+					estimatedTakeHomePay: 250
+				},
+				future: {
+					grossPay: 121153.83,
+					contribution401k: 13326.9213,
+					estimatedAGI: 97330.98070499998,
+					estimatedMAGI: 97330.98070499998,
+					estimatedTakeHomePay: 100
+				},
+				combined: {
+					grossPay: 0,
+					contribution401k: 0,
+					estimatedAGI: 0,
+					estimatedMAGI: 0,
+					estimatedTakeHomePay: 0
+				}
 			};
 		});
 		expect(result).toEqual(expectedResult);
