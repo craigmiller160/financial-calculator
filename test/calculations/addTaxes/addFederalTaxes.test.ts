958f6a6aec5c07cce7907463d3b2686d26b39487
Craig Miller <craigmiller160@gmail.com>
2022-03-11 19:01:22 -0500

    Setting up tests

--- test/calculations/addTaxes/addFederalTaxes.test.ts
+++ test/calculations/addTaxes/addFederalTaxes.test.ts
@@ -1,7 +1,20 @@
-export {};
+import input from '../../__test-data__/other/federalTaxesInput.json';
+import { PersonalDataWithTotals } from '../../../src/calculations/totals/TotalTypes';
+import { addFederalTaxes } from '../../../src/calculations/taxes/addFederalTaxes';
+import { pipe } from 'fp-ts/function';
+import { getTestData } from '../../testutils/TestData';
+import * as IOEither from 'fp-ts/IOEither';
+import '@relmify/jest-fp-ts';
+
+const personalData = input as PersonalDataWithTotals;
 
 describe('addFederalTaxes', () => {
 	it('adds federal tax info', () => {
-		throw new Error();
+		pipe(
+			getTestData(),
+			IOEither.chainEitherK(({ legalData }) =>
+				addFederalTaxes({ legalData, personalData })
+			)
+		);
 	});
 });
