c8668e47068d7a2edecbd2f91db7b87740d52130
Craig Miller <craigmiller160@gmail.com>
2022-03-11 20:05:26 -0500

    finished test

--- test/calculations/rothIRA/addRothIraEligibile.test.ts
+++ test/calculations/rothIRA/addRothIraEligibile.test.ts
@@ -1,17 +1,25 @@
-import input from '../../__test-data__/other/addRothIraLimitInput.json'
+import input from '../../__test-data__/other/addRothIraLimitInput.json';
 import { PersonalDataWithTotals } from '../../../src/calculations/totals/TotalTypes';
 import { pipe } from 'fp-ts/function';
 import { getTestData } from '../../testutils/TestData';
 import * as IOEither from 'fp-ts/IOEither';
+import { addRothIraLimit } from '../../../src/calculations/rothIRA/addRothIraLimit';
+import produce from 'immer';
+import '@relmify/jest-fp-ts';
 
-const inputData = input as PersonalDataWithTotals;
+const personalData = input as PersonalDataWithTotals;
 
 describe('addRothIraLimit', () => {
 	it('adds the limit for Roth IRA contributions', () => {
-		pipe(
+		const result = pipe(
 			getTestData(),
-
-		)
-		throw new Error();
+			IOEither.map(({ legalData }) =>
+				addRothIraLimit({ legalData, personalData })
+			)
+		)();
+		const expectedResult = produce(personalData, (draft) => {
+			draft.rothIraLimit = 2400;
+		});
+		expect(result).toEqualRight(expectedResult);
 	});
 });
