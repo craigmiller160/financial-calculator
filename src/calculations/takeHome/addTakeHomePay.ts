c41a9752b6ea871c4e2c6b1e3973d2a8ab57f1df
Craig Miller <craigmiller160@gmail.com>
2022-03-11 19:15:05 -0500

    Finished addTakeHomePay

--- src/calculations/takeHome/addTakeHomePay.ts
+++ src/calculations/takeHome/addTakeHomePay.ts
@@ -1,12 +1,30 @@
-import { BonusWithTotal, PersonalDataWithTotals } from '../totals/TotalTypes';
+import {
+	BonusWithTotal,
+	PaycheckWithTotal,
+	PersonalDataWithTotals
+} from '../totals/TotalTypes';
 import produce, { castDraft } from 'immer';
 import { pipe } from 'fp-ts/function';
 import * as RArray from 'fp-ts/ReadonlyArray';
+import Decimal from 'decimal.js';
 
 const addTakeHomeForBonus = (bonus: BonusWithTotal): BonusWithTotal =>
 	produce(bonus, (draft) => {
 		draft.estimatedTakeHomePay =
 			draft.estimatedAGI - draft.federalTaxCosts.amount;
+	});
+
+const addTakeHomeForPaycheck = (
+	paycheck: PaycheckWithTotal
+): PaycheckWithTotal =>
+	produce(paycheck, (draft) => {
+		draft.estimatedTakeHomePay =
+			draft.estimatedAGI - draft.federalTaxCost.amount;
+		draft.totalsForAllChecks.estimatedTakeHomePay = new Decimal(
+			draft.estimatedTakeHomePay
+		)
+			.times(new Decimal(draft.numberOfChecks))
+			.toNumber();
 	});
 
 export const addTakeHomePay = (
@@ -17,8 +35,18 @@ export const addTakeHomePay = (
 		data.futureBonuses,
 		RArray.map(addTakeHomeForBonus)
 	);
+	const pastPaychecks = pipe(
+		data.pastPaychecks,
+		RArray.map(addTakeHomeForPaycheck)
+	);
+	const futurePaychecks = pipe(
+		data.futurePaychecks,
+		RArray.map(addTakeHomeForPaycheck)
+	);
 	return produce(data, (draft) => {
 		draft.pastBonuses = castDraft(pastBonuses);
 		draft.futureBonuses = castDraft(futureBonuses);
+		draft.pastPaychecks = castDraft(pastPaychecks);
+		draft.futurePaychecks = castDraft(futurePaychecks);
 	});
 };
--- src/calculations/totals/TotalTypes.ts
+++ src/calculations/totals/TotalTypes.ts
@@ -32,7 +32,7 @@ export interface PaycheckTotals {
 	readonly contribution401k: number;
 	readonly estimatedAGI: number;
 	readonly estimatedMAGI: number;
-	readonly estimatedTakeHomePay: number; // TODO update with fed
+	readonly estimatedTakeHomePay: number;
 }
 
 export interface FederalTaxCosts {
@@ -49,7 +49,7 @@ export interface PaycheckWithTotal {
 	readonly numberOfChecks: number;
 	readonly estimatedAGI: number;
 	readonly estimatedMAGI: number;
-	readonly estimatedTakeHomePay: number; // TODO update with fed
+	readonly estimatedTakeHomePay: number;
 	readonly payrollTaxCost: PayrollTaxCosts;
 	readonly totalsForAllChecks: PaycheckTotals;
 	readonly federalTaxCost: FederalTaxCosts;
