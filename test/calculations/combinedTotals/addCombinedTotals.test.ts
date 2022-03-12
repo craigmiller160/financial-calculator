a74d25857e7963c43d4fa55fab143cc19858e467
Craig Miller <craigmiller160@gmail.com>
2022-03-12 11:05:56 -0500

    Fixed another test

--- test/calculations/combinedTotals/addCombinedTotals.test.ts
+++ test/calculations/combinedTotals/addCombinedTotals.test.ts
@@ -13,7 +13,7 @@ describe('addCombinedTotals', () => {
 				past: {
 					grossPay: 33710.15,
 					contribution401k: 7079.1315,
-					contributionHsa: 0,
+					contributionHsa: 192.6,
 					estimatedAGI: 23469.342025,
 					estimatedMAGI: 23469.342025,
 					estimatedTakeHomePay: 19148.971394884615
@@ -21,7 +21,7 @@ describe('addCombinedTotals', () => {
 				future: {
 					grossPay: 121153.83,
 					contribution401k: 13326.9213,
-					contributionHsa: 0,
+					contributionHsa: 807.66,
 					estimatedAGI: 97330.98070499998,
 					estimatedMAGI: 97330.98070499998,
 					estimatedTakeHomePay: 78950.56456656921
@@ -29,7 +29,7 @@ describe('addCombinedTotals', () => {
 				combined: {
 					grossPay: 154863.98,
 					contribution401k: 20406.0528,
-					contributionHsa: 0,
+					contributionHsa: 1000.26,
 					estimatedAGI: 120800.32272999999,
 					estimatedMAGI: 120800.32272999999,
 					estimatedTakeHomePay: 98099.53596145383
@@ -37,7 +37,7 @@ describe('addCombinedTotals', () => {
 				combinedWithAdditionalIncome: {
 					grossPay: 155863.98,
 					contribution401k: 20406.0528,
-					contributionHsa: 0,
+					contributionHsa: 1000.26,
 					estimatedAGI: 121800.32272999999,
 					estimatedTakeHomePay: 98099.53596145383,
 					estimatedMAGI: 121800.32272999999
