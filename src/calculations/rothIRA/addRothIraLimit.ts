d2679acf16e381fef1156389949ef907eb8d018c
Craig Miller <craigmiller160@gmail.com>
2022-03-11 20:00:07 -0500

    Wrote addRothIraLimit

--- src/calculations/rothIRA/addRothIraLimit.ts
+++ src/calculations/rothIRA/addRothIraLimit.ts
@@ -1,8 +1,26 @@
 import { PersonalDataWithTotals } from '../totals/TotalTypes';
 import { LegalData } from '../../data/decoders';
+import { pipe } from 'fp-ts/function';
+import * as RArray from 'fp-ts/ReadonlyArray';
+import * as Option from 'fp-ts/Option';
+import produce from 'immer';
 
 export const addRothIraLimit =
 	(legalData: LegalData) =>
 	(personalData: PersonalDataWithTotals): PersonalDataWithTotals => {
-		throw new Error();
+		const limit = pipe(
+			legalData.rothIraLimits,
+			RArray.findFirst(
+				(limit) =>
+					limit.maximumIncome >
+					personalData.totals.combined.estimatedMAGI
+			),
+			Option.fold(
+				() => 0,
+				(limit) => limit.contributionLimit
+			)
+		);
+		return produce(personalData, (draft) => {
+			draft.rothIraLimit = limit;
+		});
 	};
