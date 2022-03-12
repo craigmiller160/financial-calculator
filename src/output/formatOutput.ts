55e05e774ab39d6ab43b9d8bdea3614f94915b10
Craig Miller <craigmiller160@gmail.com>
2022-03-12 08:50:07 -0500

    Laid out all the things I want to output

--- src/output/formatOutput.ts
+++ src/output/formatOutput.ts
@@ -13,6 +13,27 @@ const PERCENT_FORMAT = new Intl.NumberFormat('en-US', {
 const formatPercent = (num: number): string => PERCENT_FORMAT.format(num);
 const formatCurrency = (num: number): string => CURRENCY_FORMAT.format(num);
 
+/*
+ * 1) Paycheck Info
+ * 		a) Date Range
+ * 		b) Gross Pay
+ * 		c) 401k Rate
+ * 		d) Take Home Pay
+ * 2) Bonuses
+ * 		a) Date
+ * 		b) Gross Pay
+ * 		c) 401k Rate
+ * 		d) Take Home Pay
+ * 3) Totals
+ * 		a) Gross Pay
+ * 		b) AGI/MAGI
+ * 		c) 401k Contribution
+ * 		d) Take Home Pay
+ * 4) New Stats
+ * 		a) 401k
+ * 		b) Roth IRA
+ */
+
 export const formatOutput = (data: PersonalDataWithTotals): string => {
 	const percent401k = formatPercent(data.futureRate401k);
 	const rothIraLimit = formatCurrency(data.rothIraLimit);
