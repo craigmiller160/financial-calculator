7962af9e4ab117bfb9ef31c0f552b40edd53fbc4
Craig Miller <craigmiller160@gmail.com>
2022-03-12 08:43:13 -0500

    Wrote basic output formatting

--- src/output/formatOutput.ts
+++ src/output/formatOutput.ts
@@ -1,3 +1,20 @@
-export const formatOutput = () => {
-	throw new Error();
+import { PersonalDataWithTotals } from '../calculations/totals/TotalTypes';
+
+const CURRENCY_FORMAT = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD'
+});
+const PERCENT_FORMAT = new Intl.NumberFormat('en-US', {
+	style: 'percent',
+	minimumFractionDigits: 2,
+	maximumFractionDigits: 2
+});
+
+export const formatOutput = (data: PersonalDataWithTotals): string => {
+	const percent401k = PERCENT_FORMAT.format(data.futureRate401k);
+	const rothIraLimit = CURRENCY_FORMAT.format(data.rothIraLimit);
+	return `
+	    New 401k Contribution Rate: ${percent401k}
+	    This Year's Roth IRA Limit: ${rothIraLimit}  
+	`;
 };
