0d2c73babfad5988e96c909b43d7bb75bb3ae1a9
Craig Miller <craigmiller160@gmail.com>
2022-03-11 19:57:01 -0500

    Added function types

--- src/calculations/rothIRA/addRothIraLimit.ts
+++ src/calculations/rothIRA/addRothIraLimit.ts
@@ -1,3 +1,8 @@
-export const addRothIraLimit = () => {
-	throw new Error();
-};
+import { PersonalDataWithTotals } from '../totals/TotalTypes';
+import { LegalData } from '../../data/decoders';
+
+export const addRothIraLimit =
+	(legalData: LegalData) =>
+	(personalData: PersonalDataWithTotals): PersonalDataWithTotals => {
+		throw new Error();
+	};
