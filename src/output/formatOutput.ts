ccc773796a33fd671e6553a02d9cc06a5574808f
Craig Miller <craigmiller160@gmail.com>
2022-03-12 08:47:02 -0500

    Code cleanup

--- src/output/formatOutput.ts
+++ src/output/formatOutput.ts
@@ -10,9 +10,12 @@ const PERCENT_FORMAT = new Intl.NumberFormat('en-US', {
 	maximumFractionDigits: 2
 });
 
+const formatPercent = (num: number): string => PERCENT_FORMAT.format(num);
+const formatCurrency = (num: number): string => CURRENCY_FORMAT.format(num);
+
 export const formatOutput = (data: PersonalDataWithTotals): string => {
-	const percent401k = PERCENT_FORMAT.format(data.futureRate401k);
-	const rothIraLimit = CURRENCY_FORMAT.format(data.rothIraLimit);
+	const percent401k = formatPercent(data.futureRate401k);
+	const rothIraLimit = formatCurrency(data.rothIraLimit);
 	return `
 	    New 401k Contribution Rate: ${percent401k}
 	    This Year's Roth IRA Limit: ${rothIraLimit}  
