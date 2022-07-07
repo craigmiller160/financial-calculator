57c5ed15f64a9e15f82ebf7d73d749d2d254a1d2
Craig Miller <craigmiller160@gmail.com>
2022-07-07 10:06:52 -0400

    More work on context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,13 +1,23 @@
 import { PersonalData } from '../data/decoders/personalData';
 import { LegalData } from '../data/decoders/legalData';
 import { Data } from '../data/getData';
+import { Taxes } from './taxes';
+import { Totals } from './totals';
 
-export interface Context {
+export interface BaseContext {
 	readonly personalData: PersonalData;
 	readonly legalData: LegalData;
 }
 
-export const createContext = (data: Data): Context => ({
+export interface ContextWithTaxes extends BaseContext {
+	readonly taxes: Taxes;
+}
+
+export interface ContextWithTaxesAndPastTotal extends BaseContext {
+	readonly pastTotals: Totals;
+}
+
+export const createContext = (data: Data): BaseContext => ({
 	personalData: data[0],
 	legalData: data[1]
 });
