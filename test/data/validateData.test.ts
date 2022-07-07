a2b5f01c16e92f1043ba61f4809e0bf000a0b321
Craig Miller <craigmiller160@gmail.com>
2022-07-07 09:49:22 -0400

    Implemented tests

--- test/data/validateData.test.ts
+++ test/data/validateData.test.ts
@@ -1,11 +1,59 @@
 import { getTestData } from '../testutils/getTestData';
+import { Bonus } from '../../src/data/decoders/personalData';
+import { pipe } from 'fp-ts/function';
+import * as IOEither from 'fp-ts/IOEither';
+import { validateData } from '../../src/data/validateData';
+import '@relmify/jest-fp-ts';
+import { Data } from '../../src/data/getData';
+
+const createDuplicateBonuses = (): ReadonlyArray<Bonus> => [
+	{
+		name: 'Bonus',
+		date: '2022-01-01',
+		grossPay: 100,
+		rates401k: {
+			employerRate: 0,
+			employeeRate: undefined
+		}
+	},
+	{
+		name: 'Bonus',
+		date: '2022-01-01',
+		grossPay: 100,
+		rates401k: {
+			employerRate: 0,
+			employeeRate: undefined
+		}
+	}
+];
 
 describe('validateData', () => {
 	it('data is valid', () => {
-		throw new Error();
+		const resultEither = pipe(
+			getTestData(),
+			IOEither.chainEitherK(validateData)
+		)();
+		expect(resultEither).toBeRight();
 	});
 
 	it('data has duplicate name', () => {
-		throw new Error();
+		const resultEither = pipe(
+			getTestData(),
+			IOEither.map(([personal, legal]): Data => {
+				return [
+					{
+						...personal,
+						pastBonuses: [
+							...personal.pastBonuses,
+							...createDuplicateBonuses()
+						]
+					},
+					legal
+				];
+			}),
+			IOEither.chainEitherK(validateData)
+		)();
+		// TODO validate details
+		expect(resultEither).toBeLeft();
 	});
 });
