220b474982b0217e6f2802183c9c9ca7dd3da13f
Craig Miller <craigmiller160@gmail.com>
2022-07-07 09:06:43 -0400

    Added logger

---
+++ src/logger/index.ts
@@ -0,0 +1,32 @@
+import { createLogger, format, transports } from 'winston';
+
+const myFormat = format.printf(
+	({ level, message, timestamp, stack }) =>
+		`[${timestamp}] [${level}] - ${stack ?? message}`
+);
+
+export const logger = createLogger({
+	level: 'debug',
+	levels: {
+		error: 1,
+		warn: 2,
+		info: 3,
+		debug: 4,
+		verbose: 5
+	},
+	format: format.combine(
+		format((info) => {
+			info.level = info.level.toUpperCase();
+			return info;
+		})(),
+		format.errors({
+			stack: true
+		}),
+		format.timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss.SSS'
+		}),
+		format.colorize(),
+		myFormat
+	),
+	transports: [new transports.Console()]
+});
