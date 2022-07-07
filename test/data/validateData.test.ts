ba7e40aa613c9168f43c061614a898bba14d92e5
Craig Miller <craigmiller160@gmail.com>
2022-07-07 09:45:57 -0400

    Stubbed out tests

--- test/data/validateData.test.ts
+++ test/data/validateData.test.ts
@@ -1,3 +1,11 @@
-describe('validateData', () => {
+import { getTestData } from '../testutils/getTestData';
 
+describe('validateData', () => {
+	it('data is valid', () => {
+		throw new Error();
+	});
+
+	it('data has duplicate name', () => {
+		throw new Error();
+	});
 });
