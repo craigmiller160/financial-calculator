70dab00d9c52ddceafaa1da01075be65e0513973
Craig Miller <craigmiller160@gmail.com>
2022-07-14 16:41:56 -0400

    Added file writing logic

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { pipe } from 'fp-ts/function';
 import * as IOEither from 'fp-ts/IOEither';
 import { createContext } from './context';
 import { performCalculations } from './calculations';
+import { writeDataFiles } from './output/writeDataFiles';
 
 pipe(
 	getData(),
 	IOEither.map(createContext),
 	IOEither.map(performCalculations),
+	IOEither.chain(writeDataFiles),
 	IOEither.map(() => {
 		logger.info('Working so far');
 		return null;
