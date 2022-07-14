5b46db8143f228d4b82cba0ec48b4f52a22141a3
Craig Miller <craigmiller160@gmail.com>
2022-07-14 16:48:51 -0400

    Initial test implementation

--- test/calculations/calculatePastContribution401k.test.ts
+++ test/calculations/calculatePastContribution401k.test.ts
@@ -1,7 +1,17 @@
-export {};
+import { createContext } from '../../src/context';
+import { getTestData } from '../testutils/getTestData';
+import { pipe } from 'fp-ts/function';
+import * as IOEither from 'fp-ts/IOEither';
+import { calculatePastContribution401k } from '../../src/calculations/calculatePastContribution401k';
+import '@relmify/jest-fp-ts';
 
 describe('calculatePastContribution401k', () => {
 	it('does the calculations', () => {
-		throw new Error();
+		const result = pipe(
+			getTestData(),
+			IOEither.map((data) => createContext(data)),
+			IOEither.map(calculatePastContribution401k)
+		)();
+		expect(result).toBeRight();
 	});
 });
