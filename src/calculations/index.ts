98abdc0797ac26e6722adc429e263f4d7a9a8b35
Craig Miller <craigmiller160@gmail.com>
2022-07-14 17:18:45 -0400

    Implemented calculations

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -2,8 +2,11 @@ import { Context } from '../context';
 import { calculatePastContribution401k } from './calculatePastContribution401k';
 import { pipe } from 'fp-ts/function';
 import { calculatePayrollTaxes } from './calculatePayrollTaxes';
+import { logger } from '../logger';
 
 export const performCalculations = (
 	context: Omit<Context, 'pastContribution401k' | 'payrollTaxes'>
-): Context =>
-	pipe(calculatePastContribution401k(context), calculatePayrollTaxes);
+): Context => {
+	logger.debug('Performing calculations on data');
+	return pipe(calculatePastContribution401k(context), calculatePayrollTaxes);
+};
