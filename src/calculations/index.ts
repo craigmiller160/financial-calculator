eeadc4e93ea5e7411055d6f3681ced4bb06247ed
Craig Miller <craigmiller160@gmail.com>
2022-07-14 22:24:24 -0400

    Refactoring calculation

--- src/calculations/index.ts
+++ src/calculations/index.ts
@@ -1,12 +1,15 @@
-import { Context } from '../context';
+import { BaseContext, Context } from '../context';
 import { calculatePastContribution401k } from './calculatePastContribution401k';
-import { pipe } from 'fp-ts/function';
 import { calculatePayrollTaxes } from './calculatePayrollTaxes';
 import { logger } from '../logger';
 
-export const performCalculations = (
-	context: Omit<Context, 'pastContribution401k' | 'payrollTaxes'>
-): Context => {
+export const performCalculations = (context: BaseContext): Context => {
 	logger.debug('Performing calculations on data');
-	return pipe(calculatePastContribution401k(context), calculatePayrollTaxes);
+	const pastContribution401k = calculatePastContribution401k(context);
+	const payrollTaxes = calculatePayrollTaxes(context);
+	return {
+		...context,
+		pastContribution401k,
+		payrollTaxes
+	};
 };
