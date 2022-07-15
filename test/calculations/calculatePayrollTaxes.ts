bdcf7dde689dd1c8f0c189a12a99dfdc4b21be0c
Craig Miller <craigmiller160@gmail.com>
2022-07-14 22:21:21 -0400

    Beginning work on test

--- test/calculations/calculatePayrollTaxes.ts
+++ test/calculations/calculatePayrollTaxes.ts
@@ -1,7 +1,19 @@
+import { pipe } from 'fp-ts/function';
+import { getTestData } from '../testutils/getTestData';
+import * as IOEither from 'fp-ts/IOEither';
+import { createContext } from '../../src/context';
+import { calculatePastContribution401k } from '../../src/calculations/calculatePastContribution401k';
+import { calculatePayrollTaxes } from '../../src/calculations/calculatePayrollTaxes';
+
 export {};
 
 describe('calculatePayrollTaxes', () => {
 	it('does the calculation', () => {
+		const result = pipe(
+			getTestData(),
+			IOEither.map((data) => createContext(data)),
+			IOEither.map(calculatePayrollTaxes)
+		)();
 		throw new Error();
 	});
 });
