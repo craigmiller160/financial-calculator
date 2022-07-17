475254b267b97911f30eb36d490a91b00d6481bf
Craig Miller <craigmiller160@gmail.com>
2022-07-17 12:02:10 -0400

    Implemented test

--- test/calculations/calculateFutureContribution401k.test.ts
+++ test/calculations/calculateFutureContribution401k.test.ts
@@ -1,7 +1,29 @@
-export {};
+import { getTestBaseContext } from '../testutils/getTestContext';
+import { pipe } from 'fp-ts/function';
+import * as IOEither from 'fp-ts/IOEither';
+import { calculateFutureContribution401k } from '../../src/calculations/calculateFutureContribution401k';
+import { Contribution401k } from '../../src/context/contribution401k';
+import '@relmify/jest-fp-ts';
 
 describe('calculateFutureContribution401k', () => {
-    it('does the calculation', () => {
-        throw new Error();
-    })
-})
+	it('does the calculation', () => {
+		const result = pipe(
+			getTestBaseContext(),
+			IOEither.map((context) =>
+				calculateFutureContribution401k(context, 0.2)
+			)
+		)();
+		// TODO add bonus
+		const expectedResult: Contribution401k = {
+			contributionsByBonus: [],
+			contributionsByPaycheck: [
+				{
+					name: 'Schellman 401k Check',
+					employerContribution: 288.4615,
+					employeeContribution: 1153.846
+				}
+			]
+		};
+		expect(result).toEqualRight(expectedResult);
+	});
+});
