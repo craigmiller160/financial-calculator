da0d933eef5d5d422e32a28073e55694f3eb3811
Craig Miller <craigmiller160@gmail.com>
2022-07-17 08:01:59 -0400

    Working on calculations

--- src/calculations/calculateFuture401kRate.ts
+++ src/calculations/calculateFuture401kRate.ts
@@ -1,5 +1,38 @@
 import { BaseContext } from '../context';
-import { Contribution401k } from '../context/contribution401k';
+import {
+	Contribution401k,
+	Contribution401kByItem
+} from '../context/contribution401k';
+import { Paycheck } from '../data/decoders/personalData';
+import { findNamedItemE } from '../utils/finders';
+import { pipe } from 'fp-ts/function';
+import * as Either from 'fp-ts/Either';
+import { TryT } from '@craigmiller160/ts-functions/types';
+import { times } from '../utils/decimalMath';
+
+const getTotalContributionForAllChecks = (
+	paychecks: ReadonlyArray<Paycheck>,
+	contribution: Contribution401kByItem
+): TryT<Contribution401kByItem> =>
+	pipe(
+		findNamedItemE(paychecks)(contribution.name),
+		Either.map(
+			(paycheck): Contribution401kByItem => ({
+				name: contribution.name,
+				employeeContribution: times(paycheck.numberOfChecks)(
+					contribution.employeeContribution
+				),
+				employerContribution: times(paycheck.numberOfChecks)(
+					contribution.employerContribution
+				)
+			})
+		)
+	);
+
+const getTotalPastContribution = (
+	context: BaseContext,
+	pastContributions401k: Contribution401k
+): number => {};
 
 export const calculateFuture401kRate = (
 	context: BaseContext,
