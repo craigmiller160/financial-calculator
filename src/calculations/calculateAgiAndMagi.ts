ff43e7134d4600aa7573f417a65aba94bc863228
Craig Miller <craigmiller160@gmail.com>
2022-07-17 12:13:05 -0400

    Stubbed out AGI/MAGI calculations and tests

---
+++ src/calculations/calculateAgiAndMagi.ts
@@ -0,0 +1,3 @@
+export const calculateAgiAndMagi = () => {
+	throw new Error();
+};
---
+++ test/calculations/calculateAgiAndMagi.test.ts
@@ -0,0 +1,7 @@
+export {};
+
+describe('calculateAgiAndMagi', () => {
+	it('calculates the values for each item', () => {
+		throw new Error();
+	});
+});
