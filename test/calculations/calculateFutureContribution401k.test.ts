2ac4cfdb995b6e92fcfa1d5f645b100f51924adf
Craig Miller <craigmiller160@gmail.com>
2022-07-17 12:04:17 -0400

    More test enhancements

--- test/calculations/calculateFutureContribution401k.test.ts
+++ test/calculations/calculateFutureContribution401k.test.ts
@@ -4,18 +4,43 @@ import * as IOEither from 'fp-ts/IOEither';
 import { calculateFutureContribution401k } from '../../src/calculations/calculateFutureContribution401k';
 import { Contribution401k } from '../../src/context/contribution401k';
 import '@relmify/jest-fp-ts';
+import { BaseContext } from '../../src/context';
 
 describe('calculateFutureContribution401k', () => {
 	it('does the calculation', () => {
 		const result = pipe(
 			getTestBaseContext(),
+			IOEither.map(
+				(context): BaseContext => ({
+					...context,
+					personalData: {
+						...context.personalData,
+						futureBonuses: [
+							{
+								name: 'Special Bonus',
+								date: '2022-12-31',
+								grossPay: 1000,
+								rates401k: {
+									employerRate: 0.05,
+									employeeRate: undefined
+								}
+							}
+						]
+					}
+				})
+			),
 			IOEither.map((context) =>
 				calculateFutureContribution401k(context, 0.2)
 			)
 		)();
-		// TODO add bonus
 		const expectedResult: Contribution401k = {
-			contributionsByBonus: [],
+			contributionsByBonus: [
+				{
+					name: 'Special Bonus',
+					employerContribution: 50,
+					employeeContribution: 200
+				}
+			],
 			contributionsByPaycheck: [
 				{
 					name: 'Schellman 401k Check',
